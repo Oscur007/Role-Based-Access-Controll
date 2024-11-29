17835b224c7b75806f2bbdd45b76f753d9a844d5
Baidurya <baiduryadebnath@gmail.com>
2024-11-29 22:58:54 +0530

    Adding compression

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //importing necessary dependencies
 import express from "express";
 import dotenv from "dotenv";
+import compression from "compression";
 import dbConnection from "./config/dbConnection.js";
 import authRouter from "./route/authRoute.js";
 import userRouter from "./route/userRoute.js";
@@ -12,7 +13,10 @@ const port = process.env.PORT || 7000;  // defining port
 
 dbConnection(); // connecting to the database
 
+//adding middlewares
 app.use(express.json())
+app.use(compression())
+
 // adding the routes
 app.use("/api/auth" , authRouter)
 app.use("/api/users" , userRouter)
